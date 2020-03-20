5d5793533af594fc8f8e2ae8e150958dd6d5ff0b
Acheamfour Victor Karikari <ackvictor92@gmail.com>
2020-03-20 09:50:13 +0000

    change introductory text in bikeshare.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello there! Let\'s go through some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
